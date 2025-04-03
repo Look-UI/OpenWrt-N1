bae8b7204fd23b5e641421c7dd838b803c4fe361
会飞的小情书 <71999976+Look-UI@users.noreply.github.com>
2025-04-03 09:20:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-
 ## 常用OpenWrt软件包源码合集，同步上游更新！
 ## 通用版luci适合18.06与19.07
 
@@ -29,16 +16,11 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >>feeds.conf.default
+# 添加feed源（使用echo追加）
+echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 
-
-## 常用OpenWrt软件包源码合集，同步上游更新！
-## 通用版luci适合18.06与19.07
-sed -i 'src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i 'src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/lang/golang
@@ -47,12 +29,12 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 make menuconfig
 
 
-## 解除系统限制
-ulimit -u 10000
-ulimit -n 4096
-ulimit -d unlimited
-ulimit -m unlimited
-ulimit -s unlimited
-ulimit -t unlimited
-ulimit -v unlimited
+# 解除系统限制
+for limit in u n d m s t v; do
+  case $limit in
+    u) ulimit -u 10000 ;;
+    n) ulimit -n 4096 ;;
+    *) ulimit -${limit} unlimited ;;
+  esac
+done
 
