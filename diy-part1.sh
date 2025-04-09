b8849b4e377f5be3bd056bf5ba7b12fc3eff9750
会飞的小情书 <71999976+Look-UI@users.noreply.github.com>
2025-04-09 22:18:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,10 +20,15 @@
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >> feeds.conf.default
 
-# echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 # 删除mosdns
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns && rm -rf feeds/packages/net/{alist,adguardhome,smartdns}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
+make menuconfig
 
 
 # 解除系统限制
