3e3d4527fb2eb43b00251b533a77735d044b6c62
会飞的小情书 <71999976+Look-UI@users.noreply.github.com>
2025-04-10 21:57:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,14 +18,16 @@ echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.co
 echo 'src-git fichenx https://github.com/fichenx/openwrt-package' >> feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-
+#删除冲突插件
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/lang/golang/golang
 svn co https://github.com/openwrt/packages/branches/openwrt-22.03/lang/golang/golang feeds/packages/lang/golang/golang
 ./scripts/feeds install -a 
 make menuconfig
 
-
+#删除冲突插件
+rm -rf feeds/packages/net/luci-app-fchomo
+make defconfig
 
 
 # 解除系统限制
